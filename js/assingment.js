fb2bd83686d86a58722d8f3167c401f647aba087
shakil <mdshakil2898@gmail.com>
2022-08-22 04:31:08 -0700

    calculate section added

--- js/assingment.js
+++ js/assingment.js
@@ -2,57 +2,61 @@ function changeElement (inputId){
     const selectedPlayers = document.querySelector('#selected-players');
     const newElement = document.createElement('li');
     const playerName = document.getElementById(inputId).innerText;
-    newElement.innerText = playerName;
+    const a = document.querySelectorAll('#selected-players li').length;
+    if(a < 5){
+        newElement.innerText = playerName;
     selectedPlayers.appendChild(newElement);
+    }
+    else{
+        alert('you can not choose over 5 player');
+    }
+    
 }
 function deleteElement(){
     const selectedPlayers = document.querySelector('#selected-players');
      selectedPlayers.removeChild(li[5]);
 }
 function disabledButton(inputId){
-    const Btn = document.getElementById(inputId); 
-    Btn.setAttribute('disabled', '');
-}
-function maximumSelectPlayer (){
-    const a = document.querySelectorAll('#selected-players li');
-    const b= a.length;
-    if(b > 5){
-        alert('you cannot choose more than 5 players');
-        return false;
-    }
-    else{
-        
-    }
+    const eventButton = document.getElementById(inputId); 
+    eventButton.setAttribute('disabled', '');
 }
 
 document.getElementById('player1-btn').addEventListener('click',function(){
-    changeElement("player1-name");
     disabledButton('player1-btn');
-    maximumSelectPlayer();
+    changeElement("player1-name");
+    
 })
 document.getElementById('player2-btn').addEventListener('click',function(){
-    changeElement('player2-name');
     disabledButton('player2-btn');
-    maximumSelectPlayer();
+    changeElement('player2-name');
+    
 })
 document.getElementById('player3-btn').addEventListener('click',function(){
-    changeElement('player3-name');
     disabledButton('player3-btn');
-    maximumSelectPlayer();
+    changeElement('player3-name');
+    
 })
 document.getElementById('player4-btn').addEventListener('click',function(){
-    changeElement('player4-name');
     disabledButton('player4-btn');
-    maximumSelectPlayer();
+    changeElement('player4-name');
+    
 })
 document.getElementById('player5-btn').addEventListener('click',function(){
-    changeElement('player5-name');
     disabledButton('player5-btn');
-    maximumSelectPlayer();
+    changeElement('player5-name');
+    
 })
 document.getElementById('player6-btn').addEventListener('click',function(){
-    changeElement('player6-name');
     disabledButton('player6-btn');
-    maximumSelectPlayer();
+    changeElement('player6-name');
+    
+})
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const playerCost = document.getElementById('player-cost').value;
+    const playerList = document.querySelectorAll('#selected-players li').length;
+    const playerExpenseElement = document.getElementById('player-expenses');
+    const totalPlayerExpenses = playerCost * playerList;
+    playerExpenseElement.innerText = totalPlayerExpenses;
 })
 
